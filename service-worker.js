9dbf6f3453fa3feb1920eeb416806b6a25b11257
AteCzamor <c.zamorano.atento@gmail.com>
2026-07-29 15:46:37 -0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+
+const CACHE = 'camilo-card-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css',
+  './app.js',
+  './manifest.webmanifest',
+  './camilo-zamorano.vcf',
+  './assets/movistar-logo.png',
+  './assets/qr-contacto.png'
+];
+self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS))));
+self.addEventListener('fetch', event => event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))));
