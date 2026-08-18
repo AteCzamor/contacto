06ee93b73c9cec72b675ee59bdbcc422e38e539b
AteCzamor <c.zamorano.atento@gmail.com>
2026-08-17 22:54:08 -0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,5 +0,0 @@
-const CACHE_NAME = 'camilo-card-v2';
-const ASSETS = ['./','index.html','styles.css','app.js','manifest.webmanifest','camilo-zamorano.vcf','assets/fondo-concepcion.png','assets/movistar-logo.png','assets/qr-contacto.png'];
-self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
-self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))).then(() => self.clients.claim())); });
-self.addEventListener('fetch', event => { event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request))); });
